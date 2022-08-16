742aba344154ee4294a4a2fd2e59a72ec686fc71
Halime Lafcı <halimebabacan9@gmail.com>
2022-08-16 22:09:22 +0300

    c01

--- src/Day3_GitGithup.java
+++ src/Day3_GitGithup.java
@@ -3,5 +3,7 @@ public class Day3_GitGithup {
     public static void main(String[] args) {
 
         System.out.println("3. gun 1. deneme");
+        System.out.println("hersey silinmis");
+        System.out.println("yenisini yapıcaz mecbur");
     }
 }
